7d223ee3cfc3408045aebc8230e7aa5c6cbf90f6
Wallace <rwwalla2@illinois.edu>
2020-02-18 14:53:55 -0600

    generate package

--- vrtg-cli.go
+++ vrtg-cli.go
@@ -16,21 +16,22 @@ import (
 
 func main() {
 
-	// cwd, err := os.Getwd()
-	// if err != nil {
-	// 	log.Println(err)
-	// }
+	OUTPUT := "out/"
+	OUTPUT_STATIC := OUTPUT + "static/"
+	OUTPUT_IMG_DIR := OUTPUT_STATIC + "img/"
 
-	outputDir := "static/"
-	outputImgDir := outputDir + "img/"
+	TEMPLATES := "templates/"
+	TEMPLATES_STATIC := TEMPLATES + "static/"
+
+	_ = os.MkdirAll(OUTPUT_IMG_DIR, 0666)
 
 	dir := flag.String("p", "NONE", "the path to the directory of 360 images")
 	title := flag.String("t", "360 Tour", "the title for the generated tour")
 	columns := flag.Int("c", 1, "the number of columns in the image matrix")
-	rows := flag.Int("r", 1, "the number of rows in the image matrix")
+	// rows := flag.Int("r", 1, "the number of rows in the image matrix")
 
 	flag.Parse()
-	fmt.Println("args:", *dir, *columns, *rows, *title)
+	// fmt.Println("args:", *dir, *columns, *rows, *title)
 
 	if *dir == "NONE" {
 		fmt.Println("please provide the path to the image directory")
@@ -42,59 +43,83 @@ func main() {
 
 	pathErr := filepath.Walk(*dir, func(path string, info os.FileInfo, err error) error {
 		files = append(files, path)
+		fileCounter++
 		return nil
 	})
 	if pathErr != nil {
 		panic(pathErr)
 	}
-	for _, file := range files {
-		fmt.Println(file)
-		fileCounter++
-	}
 
 	fmt.Println("Number of files:", fileCounter)
 
-	matrixRows := fileCounter / *columns
-	fmt.Println("Image matrix is:", matrixRows, "by", *columns)
+	var matrixRows int
+	if *columns > 1 {
+		matrixRows = fileCounter / *columns
+	} else {
+		matrixRows = fileCounter
+	}
+	fmt.Println("Image matrix is:", matrixRows, "row(s) by", *columns, "column(s)")
 
 	var fileIndex = 1
+	fmt.Println("Copy and rename images -->", OUTPUT_IMG_DIR)
 	for r := 1; r <= matrixRows; r++ {
 		for c := 1; c <= *columns; c++ {
-			fmt.Println(r, c)
-			copyImg(outputImgDir, files[fileIndex], r, c)
+			copyAndRenameImg(OUTPUT_IMG_DIR, files[fileIndex], r, c)
 			fileIndex++
 		}
 	}
 
-	htmlTemplatePath := "template.html"
+	var static_files []string
+	ts_pathErr := filepath.Walk(TEMPLATES_STATIC, func(path string, info os.FileInfo, err error) error {
+		static_files = append(static_files, path)
+		return nil
+	})
+	if ts_pathErr != nil {
+		panic(ts_pathErr)
+	}
+
+	for p := 1; p < len(static_files); p++ {
+		filename := strings.Replace(static_files[p], "templates", "out", 1)
+		copyFile(static_files[p], filename)
+		fmt.Println(static_files[p], "-->", filename)
+	}
+
+	fmt.Println("Format and generate index.html...")
+	htmlTemplatePath := TEMPLATES + "template.html"
 	htmlTemplateText := readHtmlTemplate(htmlTemplatePath)
 
 	html := strings.Replace(htmlTemplateText, "{{num_rows}}", strconv.Itoa(matrixRows), 1)
 	html = strings.Replace(html, "{{num_cols}}", strconv.Itoa(*columns), 1)
-	html = strings.Replace(html, "{{img_dir}}", "'"+outputImgDir+"'", 1)
+	html = strings.Replace(html, "{{img_dir}}", "'"+OUTPUT_IMG_DIR+"'", 1)
 	html = strings.Replace(html, "{{img_ext}}", "'.jpg'", 1)
-	fmt.Println(html)
+	html = strings.Replace(html, "{{title}}", *title, 1)
 
-	err := ioutil.WriteFile("index.html", []byte(html), 0644)
+	err := ioutil.WriteFile(OUTPUT+"index.html", []byte(html), 0644)
 	if err != nil {
 		fmt.Println(err)
 	}
 
+	fmt.Println("Done.")
+
 }
 
-func copyImg(outputImgDir string, path string, r int, c int) string {
-	from, err := os.Open(path)
+func copyAndRenameImg(outputImgDir string, path string, r int, c int) string {
+	ext := strings.Split(path, ".")
+	var newPath string
+	newPath = outputImgDir + strconv.Itoa(r) + "_" + strconv.Itoa(c) + "." + ext[1]
+	copyFile(path, newPath)
+	return newPath
+}
+
+func copyFile(src string, dst string) {
+
+	from, err := os.Open(src)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer from.Close()
 
-	ext := strings.Split(path, ".")
-
-	var newPath string
-	newPath = outputImgDir + strconv.Itoa(r) + "_" + strconv.Itoa(c) + "." + ext[1]
-
-	to, err := os.OpenFile(newPath, os.O_RDWR|os.O_CREATE, 0666)
+	to, err := os.OpenFile(dst, os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -104,8 +129,6 @@ func copyImg(outputImgDir string, path string, r int, c int) string {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	return newPath
 }
 
 func readHtmlTemplate(path string) string {
@@ -114,7 +137,6 @@ func readHtmlTemplate(path string) string {
 		log.Fatal(err)
 	}
 
-	// Convert []byte to string and print to screen
 	text := string(content)
 	return text
 }
