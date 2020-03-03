839b77a575a8c6851f68bcdf3522e7816016b411
Wallace <rwwalla2@illinois.edu>
2020-03-03 11:59:20 -0600

    materialize framwork

--- app/renderer.js
+++ app/renderer.js
@@ -11,7 +11,7 @@ const package = document.getElementById('packageLoader')
 
 package.addEventListener('change', (event) => {
   const modalPath = package.files[0].path;
-  let win = new BrowserWindow({ width: 400, height: 320 })
+  let win = new BrowserWindow({ width: 800, height: 600 })
 
   win.on('close', () => { win = null })
   win.loadURL(modalPath)
