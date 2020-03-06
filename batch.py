af9cfa4235ce1e77a35a60a0b785ae6a17cd7087
Wallace <rwwalla2@illinois.edu>
2020-03-06 16:50:32 -0600

    reverse tours

--- batch.py
+++ batch.py
@@ -8,7 +8,15 @@ for dirName, _, _ in os.walk(imgDir):
     name = dirName.split('/')[-1]
     args = ['./ginger.exe', '-p=' + dirName, '-t=' + name, '-o=' + outDir]
     if name != '':
-        if name in ['Foellinger Auditorium', 'Loomis 151']:
+        if name in [
+            'Foellinger Auditorium', 
+            'Loomis 151', 
+            'Lincoln Hall Theater', 
+            'Siebel Center 1404', 
+            'National Soybean 149',
+            'Temple-Buell Hall 134',
+            'David Kinley Hall 114'
+            ]:
             print('Processing: ' + name)
             args.append('-r')
             subprocess.run(args)
