c493d2b68e89ed7dd09a07891d958a94165fb765
unknown <rwwalla2@illinois.edu>
2020-04-21 16:19:27 -0500

    using styled open button

--- editor/save.js
+++ editor/save.js
@@ -14,6 +14,9 @@ function saveConfig() {
             config[key][skey][i][k] = intermediateConfig[k];
         }
 
+        // reset intermediateConfig object
+        intermediateConfig = {};
+
         console.log('updated config:', k, config[key][skey][i][k])
 
         let configText = "var config = "
