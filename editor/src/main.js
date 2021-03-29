bae40e8bfe43f344e8bcc84141299b9551ba42ff
Robert Wallace <rwwalla2@illinois.edu>
2021-03-29 17:39:59 -0500

    deps, devtools broken in electron 6

--- editor/src/main.js
+++ editor/src/main.js
@@ -5,8 +5,8 @@ import 'materialize-css/dist/css/materialize.min.css'
 import 'material-design-icons/iconfont/material-icons.css'
 
 
-Vue.config.productionTip = false
-Vue.config.devtools = true
+// Vue.config.productionTip = false
+// Vue.config.devtools = true
 
 new Vue({
   render: h => h(App),
